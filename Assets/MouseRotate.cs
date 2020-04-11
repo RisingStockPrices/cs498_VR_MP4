30412f6567dce4362bd4aa771eca66438a3ec857
RisingStockPrices <thswndms98@naver.com>
2020-04-12 03:43:30 +0900

    Completed the Start Menu
    added start menu scene with transition to game scene
    +animations/triggers

---
+++ Assets/MouseRotate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseRotate : MonoBehaviour
+{
+    public float mouseSens = 100f;
+    public Transform playerT;
+
+    float xRot = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+        playerT.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    float speed = 5f;
+    bool isMoving = false;
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+
+        if (x!=0 || z!=0) //movement
+        {
+
+            Vector3 translate = transform.right * x + transform.forward * z;
+            controller.Move(translate * speed);
+            isMoving = true;
+           
+        }
+        else
+        {
+            isMoving = false;
+        }
+
+        anim.SetBool("IsMoving", isMoving);
+    }
+     void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag=="StartGame")
+        {//jump to game scene
+            SceneManager.LoadScene(sceneName: "CS498HW4");
+        }
+        else if(collision.gameObject.tag=="EndGame")
+        {
+            print("attempting to quit");
+            Application.Quit();
+        }
+    }
+}
