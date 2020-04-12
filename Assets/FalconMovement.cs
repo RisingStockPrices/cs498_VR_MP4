7b6b2077cb4c195a89a1a1efe00a8ac8c7ce6e9d
RisingStockPrices <thswndms98@naver.com>
2020-04-12 10:23:14 +0900

    control looks good!
    ws : accel
    ad : yaw
    tg : pitch
    hf : roll

---
+++ Assets/FalconMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FalconMovement : MonoBehaviour
+{
+    CharacterController controller;
+    public float speed = 200f;
+    public float minSpeed = 100f;
+    public float maxSpeed = 300f;
+    public float boostSpeed = 500f;
+    float speedSensitivity = 2f;
+    float turnSensitivity = .5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float accel = Input.GetAxis("Vertical");
+        float yaw = Input.GetAxis("Horizontal");
+        float pitch = Input.GetAxis("Fire1"); // x axis, controls: TG
+        float roll = Input.GetAxis("Fire2"); // z axis, controls : HF
+
+        //Vector3 rot = transform.forward * roll + transform.up * yaw + transform.right * pitch;
+        Vector3 rot = Vector3.back * roll + Vector3.up * yaw + Vector3.left * pitch;
+        transform.Rotate(rot * turnSensitivity);
+
+        speed += accel * speedSensitivity;
+        speed = Mathf.Clamp(speed, minSpeed, maxSpeed);
+
+        float befBoostSpeed = speed;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            speed = boostSpeed;
+
+        Vector3 translate = transform.forward * speed * Time.deltaTime;
+        controller.Move(translate);
+
+        speed = befBoostSpeed;
+    }
+}
